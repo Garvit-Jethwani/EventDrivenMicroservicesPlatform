ec98cfdbe5d0e7bf4308ac714ccdbe0d7fd7bc5a
Garvit-Jethwani <garvitjethwani@gmail.com>
2024-06-13 00:50:06 +0530

    Order service implemented

---
+++ order-service/events/handler.go
@@ -0,0 +1,6 @@
+
+package events
+
+func HandleOrderCreatedEvent(orderID string) {
+    // To be implemented: handle order_created events, e.g., update inventory
+}
---
+++ order-service/events/producer.go
@@ -0,0 +1,48 @@
+
+package events
+
+import (
+    "encoding/json"
+    "order-service/models"
+    "github.com/segmentio/kafka-go"
+    "log"
+)
+
+const (
+    topic     = "order_events"
+    partition = 0
+)
+
+var writer *kafka.Writer
+
+func InitEventProducer(brokers []string) {
+    writer = &kafka.Writer{
+        Addr:     kafka.TCP(brokers...),
+        Topic:    topic,
+        Balancer: &kafka.LeastBytes{},
+    }
+}
+
+func ProduceOrderCreatedEvent(order *models.Order) error {
+    event := map[string]interface{}{
+        "event": "order_created",
+        "data":  order,
+    }
+    payload, err := json.Marshal(event)
+    if err != nil {
+        return err
+    }
+
+    err = writer.WriteMessages(nil, kafka.Message{
+        Value: payload,
+    })
+    if err != nil {
+        log.Printf("could not write message: %v
+", err)
+        return err
+    }
+
+    log.Printf("order created event produced: %v
+", order)
+    return nil
+}
